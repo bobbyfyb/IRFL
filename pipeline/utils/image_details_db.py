d4ea196f38027d38d8190850186b6e9a76bfff08
Artorias <fyb0118@gmail.com>
2024-11-26 13:20:41 +0900

    feat: change region of aws to tokyo

--- pipeline/utils/image_details_db.py
+++ pipeline/utils/image_details_db.py
@@ -14,8 +14,8 @@ from pipeline.utils.utils import get_str_hash
 class ImageDetailsDB:
 
     def __init__(self, use_local_cache=True):
-        self.client = boto3.client('dynamodb', aws_access_key_id=os.environ['AWS_ID'], aws_secret_access_key=os.environ['AWS_SECRET'], region_name='us-east-1')
-        self.dynamodb = boto3.resource('dynamodb', aws_access_key_id=os.environ['AWS_ID'], aws_secret_access_key=os.environ['AWS_SECRET'], region_name='us-east-1')
+        self.client = boto3.client('dynamodb', aws_access_key_id=os.environ['AWS_ID'], aws_secret_access_key=os.environ['AWS_SECRET'], region_name='ap-northeast-1')
+        self.dynamodb = boto3.resource('dynamodb', aws_access_key_id=os.environ['AWS_ID'], aws_secret_access_key=os.environ['AWS_SECRET'], region_name='ap-northeast-1')
         self.dynamoDB_Cache = dict()
         if use_local_cache:
             self.init_dynamoDB_cache()
